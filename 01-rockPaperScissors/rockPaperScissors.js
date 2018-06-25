0bb9d4346ae94c4c97f2410faa87817ba9fa8ea7
세진 <2nd@MacBook-Pro-40.local>
2018-06-25 22:43:46 +0900

    refractor 1st toy

--- 01-rockPaperScissors/rockPaperScissors.js
+++ 01-rockPaperScissors/rockPaperScissors.js
@@ -1,38 +1,3 @@
-var ArrayProto = Array.prototype;
-var nativeMap = ArrayProto.map;
-var _ = function(obj) {
-  if (obj instanceof _) return obj;
-  if (!(this instanceof _)) return new _(obj);
-  this._wrapped = obj;
-};
-_.range = function(start, stop, step) {
-  if (arguments.length <= 1) {
-    stop = start || 0;
-    start = 0;
-  }
-  step = arguments[2] || 1;
-
-  var len = Math.max(Math.ceil((stop - start) / step), 0);
-  var idx = 0;
-  var range = new Array(len);
-
-  while(idx < len) {
-    range[idx++] = start;
-    start += step;
-  }
-
-  return range;
-};
-
-_.map = _.collect = function(obj, iterator, context) {
-  var results = [];
-  if (obj == null) return results;
-  if (nativeMap && obj.map === nativeMap) return obj.map(iterator, context);
-  each(obj, function(value, index, list) {
-    results[results.length] = iterator.call(context, value, index, list);
-  });
-  return results;
-};
 /*
 * Write a function that generates every sequence of throws a single
 * player could throw over a three-round game of rock-paper-scissors.
@@ -54,12 +19,11 @@ _.map = _.collect = function(obj, iterator, context) {
 
 var rockPaperScissors = function (n) {
   // TODO: your solution here
-  var i = 0;
-  var result = [];
-  var toTernary = function(num){
-    var ternary = '';
-    var limit= Math.pow(3,n-1);
-    var tc=0;
+  let result = [];
+  const toTernary = num => {
+    let ternary = '';
+    let limit= Math.pow(3,n-1);
+    let tc=0;
     while(num>0){ 
       if(num>=limit){
         num-=limit;
@@ -72,41 +36,24 @@ var rockPaperScissors = function (n) {
     }
     return ternary+tc;
   };
-  result = _.range(0,Math.pow(3,n)-1)
-  .map( x=>toTernary(x))
-  .map(function (x) {
-    x=x+'';
-    while(x.length<n) {
-      x+='0';
-    }
-    return x;
-  })
-  .map(function (context, value, index, list){
-    var x = [];
-    for(i in context){
-      if(context[i] === '0') x.push('rock');
-      else if(context[i] === '1') x.push('paper');
-      else if(context[i] === '2') x.push('scissors');
-    }
-    return x;     
-  });
-  // while(n > 0){
-  //   for(j=0;j<n;j++){
-  //     iterator();
-  //   }
-  //   n--;
-  // }
-  // for(i in rPS){
-  //   for(j in rPS){
-  //     for(k in rPS){
-  //       for(l in rPS){
-  //         for(m in rPS){
-  //           result.push([rPS[i],rPS[j],rPS[k],rPS[l],rPS[m]]);
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+  result = Array.from({length: Math.pow(3,n)-1}, (v, i) => i)
+    .map(x => toTernary(x))
+    .map(x => {
+      x=x+'';
+      while(x.length<n) {
+        x+='0';
+      }
+      return x;
+    })
+    .map( el => {
+      let x = [];
+      for(let i in el){
+        if(el[i] === '0') x.push('rock');
+        else if(el[i] === '1') x.push('paper');
+        else if(el[i] === '2') x.push('scissors');
+      }
+      return x;     
+    });
   return result;
 };
 
