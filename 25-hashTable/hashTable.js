e252bdd3e079f38c6e2e8d7cfb632c3e1e48ba61
세진 <2nd@MacBook-Pro-40.local>
2018-07-14 17:43:57 +0900

    25th toy complete

--- 25-hashTable/hashTable.js
+++ 25-hashTable/hashTable.js
@@ -7,19 +7,46 @@ var makeHashTable = function(){
   var result = {};
   var storage = [];
   var storageLimit = 1000;
-  result.insert = function(/*...*/ 
-){
-    // TODO: implement `insert()`
+  result.insert = function(str){
+    const key = getIndexBelowMaxForKey(str, storageLimit);
+    if( storage[key] ) {
+      let next = storage[key];
+      while(next[1]) {
+        next = next[1];
+      }
+      next[1] = [str, null];
+    } else {
+      storage[key] = [str, null];
+    }
+    return key;
   };
 
-  result.retrieve = function(/*...*/ 
-){
-    // TODO: implement `retrieve()`
+  result.retrieve = function(str){
+    const key = getIndexBelowMaxForKey(str, storageLimit);
+    let next = storage[key];
+    while (next) {
+      if(next[0] === str)
+        return str;
+      next = next[1];
+    }
+    return false;
   };
 
-  result.remove = function(/*...*/ 
-){
-    // TODO: implement `remove()`
+  result.remove = function(str){
+    const key = getIndexBelowMaxForKey(str, storageLimit);
+    let next = storage[key];
+    if(next[0] === str) {
+      storage[key] = next[1];
+      return true;
+    }
+    while (next[1]) {
+      if(next[1][0] === str) {
+        next[1] = next[1][1];
+        return true;
+      }
+      next = next[1];
+    }
+    return false;
   };
 
   return result;
@@ -37,3 +64,61 @@ var getIndexBelowMaxForKey = function(str, max){
   }
   return hash % max;
 };
+
+const myHash = makeHashTable();
+myHash.insert("injured");
+myHash.insert("shrinking");
+myHash.insert("painless");
+myHash.insert("hitchhiker");
+myHash.insert("leverage");
+myHash.insert("shameful");
+myHash.insert("enormous");
+myHash.insert("honorless");
+myHash.insert("allowed");
+myHash.insert("tangible");
+myHash.insert("unavoidable");
+myHash.insert("augur");
+myHash.insert("bellicose");
+myHash.insert("revere");
+myHash.insert("preposterous");
+myHash.insert("piquant");
+myHash.insert("loyalist");
+myHash.insert("magnanimous");
+myHash.insert("redoubtable");
+myHash.insert("immutable");
+myHash.insert("orthodox");
+myHash.insert("aversion");
+myHash.insert("vague");
+myHash.insert("verisimilar");
+myHash.insert("byzantine");
+myHash.insert("backbreaking");
+myHash.insert("diverge");
+myHash.insert("premeditated");
+myHash.insert("spontaneous");
+myHash.insert("tranquilize");
+myHash.insert("enslave");
+myHash.insert("fertile");
+myHash.insert("inauspicious");
+myHash.insert("gainsay");
+myHash.insert("undergirder");
+console.log(myHash.retrieve("inevitable"));
+// console.log(myHash.remove("unavoidable"));
+console.log(myHash.retrieve("unavoidable"));
+// console.log(myHash.remove("augur"));
+console.log(myHash.retrieve("augur"));
+console.log(myHash.remove("inauspicious"));
+console.log(myHash.retrieve("inauspicious"));
+
+const duplicate = (...arr) => {
+  let obj = {};
+  arr.forEach( val => {
+    let key = getIndexBelowMaxForKey(val,1000)
+    
+    obj[key] = obj[key]? obj[key].push(val) :[val];
+  });
+  // console.log(arr.length ,Object.keys(obj).length);
+  // console.log(obj[]);
+  
+  return obj;
+}
+
