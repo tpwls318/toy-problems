8c1f004dee05ce18dbe8b847d32e815f12e8d01a
세진 <2nd@MacBook-Pro-40.local>
2018-07-16 14:35:34 +0900

    26th toy complete

--- 26-linkedList/linkedList.js
+++ 26-linkedList/linkedList.js
@@ -19,21 +19,46 @@
 
 var LinkedList = function(){
   //fill me in!
+  this.head = null;
+  this.tail = null;
 };
 
 //write methods here!
 
-LinkedList.prototype.addToTail = function(
-){
+LinkedList.prototype.addToTail = function(value){
+  let node = this.makeNode(value);
+  if(!this.head) this.head = node;
+  if(this.tail) this.tail.next = node;
+  this.tail = node;
 };
 
 LinkedList.prototype.removeHead = function(){
+  if(this.head){
+    const removedValue = this.head.value;
+    this.head = this.head.next;
+    return removedValue;
+  } 
+  return false;
 };
 
-LinkedList.prototype.contains = function(
-){
+LinkedList.prototype.contains = function(value){
+  let curNode = this.head;
+  while (curNode) {
+    if(curNode.value === value) return true;
+    curNode = curNode.next;
+  }
+  return false;
 };
 
-LinkedList.prototype.makeNode = function(
-){
-};
+LinkedList.prototype.makeNode = 
+  (value) => ({value, next: null});
+
+var list = new LinkedList();
+console.log(list.tail);  //yields 'null'
+list.addToTail(4);
+list.addToTail(5);
+console.log(list.head.value);   //yields '4';
+console.log(list.contains(5));  //yields 'true';
+console.log(list.contains(6));  //yields 'false';
+console.log(list.removeHead());  //yields '4'
+console.log(list.tail.value);  //yields '5';
