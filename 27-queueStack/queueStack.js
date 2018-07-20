56b8df14f5c1661837b5fed8ef3726ffe0bfa5c4
세진 <2nd@MacBook-Pro-40.local>
2018-07-20 18:10:50 +0900

    27th toy complete

--- 27-queueStack/queueStack.js
+++ 27-queueStack/queueStack.js
@@ -8,19 +8,35 @@
   * Stack Class
   */
 var Stack = function() {
-
+  this.storage = {};
+  this.index = 0;
   // add an item to the top of the stack
-  this.push = function(){
+  this.push = function(item){
+    this.storage[this.index] = item;
+    return ++this.index;
   };
 
   // remove an item from the top of the stack
   this.pop = function(){
+    let item = this.storage[--this.index];
+    delete this.storage[this.index];
+    return item;
   };
 
   // return the number of items in the stack
   this.size = function(){
+    return this.index;
   };
 };
+let stack = new Stack();
+console.log(
+  stack.push("foo"),
+  stack.push("bar"),
+  stack.pop(),
+  stack.size(),
+  stack.pop(),
+  stack.size(),
+);
 
 /**
   * Queue Class
@@ -31,17 +47,43 @@ var Queue = function() {
   var outbox = new Stack();
 
   // called to add an item to the `queue`
-  this.enqueue = function(){
+  this.enqueue = function(item){
     // TODO: implement `enqueue`
+    inbox.push(item);
   };
 
   // called to remove an item from the `queue`
   this.dequeue = function(){
     // TODO: implement `dequeue`
+    if(!outbox.size()){
+      while(inbox.size() > 1){
+        outbox.push(inbox.pop())
+      }
+      return inbox.pop();
+    }
+    return outbox.pop();
   };
 
   // should return the number of items in the queue
   this.size = function(){
     // TODO: implement `size`
+    return inbox.size() + outbox.size();
   };
 };
+
+let queue = new Queue();
+
+queue.enqueue("foo");
+queue.enqueue("bar");
+queue.enqueue("car");
+console.log(
+  queue.dequeue(),
+  queue.size(),
+  queue.dequeue(),
+  queue.size(),
+);
+queue.enqueue("baz");
+console.log(
+  queue.dequeue(),
+  queue.size(),
+);
