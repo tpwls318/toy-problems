5be143d36ddb218e9be58d80839498e385796e47
세진 <2nd@MacBook-Pro-40.local>
2018-07-26 18:18:28 +0900

    29th toy complete

--- 29-robotPaths/robotPaths.js
+++ 29-robotPaths/robotPaths.js
@@ -28,6 +28,42 @@ var makeBoard = function(n) {
   return board;
 };
 
+
 var robotPaths = function(n, board, i, j) {
+  board = new board(n);
+  let count = 0;
+  const traverse = (n, board, i, j) => {
+    let toGo = [i, j];
+    if(i === n - 1 && j === n - 1){
+      count++;
+      return toGo;
+    }
+    if(i > 0 && !board.hasBeenVisited(i - 1, j)){
+      toGo = [i - 1, j];
+      board.togglePiece(...toGo);
+      traverse(n, board, i - 1, j);
+      board.togglePiece(...toGo);
+    } if(j < n - 1 && !board.hasBeenVisited(i, j + 1)){
+      toGo = [i, j + 1];
+      board.togglePiece(...toGo);
+      traverse(n, board, i, j + 1);
+      board.togglePiece(...toGo);
+    } if(i < n - 1 && !board.hasBeenVisited(i + 1, j)){
+      toGo = [i + 1, j];
+      board.togglePiece(...toGo);
+      traverse(n, board, i + 1, j);
+      board.togglePiece(...toGo);
+    } if(j > 0 && !board.hasBeenVisited(i, j - 1)){
+      toGo = [i, j - 1];
+      board.togglePiece(...toGo);
+      traverse(n, board, i, j - 1);
+      board.togglePiece(...toGo);
+    }
+    return toGo;
+  }
+  board.togglePiece(i, j);
+  traverse(n, board, i, j);
+  return `possible unique paths: ${count}`
 }
 
+console.log(robotPaths(5, makeBoard, 0, 0));
